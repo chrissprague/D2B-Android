58b8982a3ccc062a1c234c6820204de2405fe242
Chris <secretevilradio@gmail.com>
2014-08-13 22:01:57 -0400

    remove unused imports

--- src/com/css/d2bAndroid/MainActivity.java
+++ src/com/css/d2bAndroid/MainActivity.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
