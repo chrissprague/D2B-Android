d6477257f8d146d0f85a09cdd9423a5d72094328
chris <christopherssprague@gmail.com>
2014-08-23 17:34:52 -0400

    fixed cron stuff, hopefully

--- src/.symlinkLatestVersion.sh
+++ src/.symlinkLatestVersion.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 LATEST=`find Versions/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "`
+echo "$LATEST" >> "cron.log"
 rm "D2B-Android.apk"
 ln -P $LATEST D2B-Android.apk
 chmod o+rx D2B-Android.apk
+echo $(date) >> "cron.log"
+echo "Daily link success" >> "cron.log"
+echo "" >> "cron.log"
 
